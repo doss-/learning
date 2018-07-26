c8e720c22e5ee2bc181756601b2d32cc7cf479b1
doss- <1234dos@gmail.com>
2018-07-26 03:51:24 +0300

    try_flask: trying to get values of submitted stuff

--- Dockerfiles/docker-compose/first_try/app/appFlask.py
+++ Dockerfiles/docker-compose/first_try/app/appFlask.py
@@ -3,15 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-from flask_wtf import Form
-from wtforms import IntegerField, TextField, SubmitField
+#import WTForms
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, SubmitField
 
 app.secret_key = 'development key'
 
-class SubmitForm(Form):
-  text_field = TextField(u'Text Field:')
+class SubmitForm(FlaskForm):
+  text_field = StringField("Text Field:")
   int_field = IntegerField(u'Integer Field:')
-  submit = SubmitField(u'ClickMe')
+  submit_button = SubmitField(u'ClickMe!')
 
 from flask import render_template
 
@@ -24,11 +25,12 @@ def renderMe():
 #display all values from db table in there
 
 
-#@app.route(u'/submit', methods=[u'POST'])
-#def submitMethod():
-#  print("test")
-#  form = SubmitForm()
-#  if form.valudate_on_submit():
+@app.route(u'/submit', methods=[u'POST'])
+def submitMethod():
+  form = SubmitForm()
+  #if form.valudate_on_submit():
+  return_message = "text field:{} ; int field:{}".format(form.text_field, form.int_field)
+  return return_message
     
 if __name__ == "__main__":
   app.run(host = '0.0.0.0')

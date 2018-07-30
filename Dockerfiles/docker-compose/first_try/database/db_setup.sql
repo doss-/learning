c9fdff9645147244c5cf0e1d2fde94af8219680e
doss- <1234dos@gmail.com>
2018-07-31 02:36:17 +0300

    db connect try#1 [missed the file]

--- Dockerfiles/docker-compose/first_try/database/db_setup.sql
+++ Dockerfiles/docker-compose/first_try/database/db_setup.sql
@@ -1,5 +1,5 @@
 /*create user 'tuser'@'localhost' identified by 'testp';
 grant all on *.* to 'tuser'@'localhost';
 create schema testdb;*/
-CREATE TABLE IF NOT EXISTS testdb.t (c CHAR(20));
-INSERT testdb.t (c) VALUE ('first');
+CREATE TABLE IF NOT EXISTS testdb.t (c CHAR(20), ci INT);
+INSERT testdb.t (c) VALUE ('first', 1);

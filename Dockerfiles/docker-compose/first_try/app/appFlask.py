ac9b1e00ad13dc0137e8e627462558b0301aec68
doss- <1234dos@gmail.com>
2018-07-31 02:35:35 +0300

    db connect try#1

--- Dockerfiles/docker-compose/first_try/app/appFlask.py
+++ Dockerfiles/docker-compose/first_try/app/appFlask.py
@@ -1,7 +1,29 @@
+# http://docs.sqlalchemy.org/en/rel_0_9/dialects/mysql.html#module-sqlalchemy.dialects.mysql.mysqlconnector
+# mysql wrappers.. seems like i need one anyway
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+dbuser = "testu"
+dbpasswd = "testpass"
+dbserver = "mysql_server"
+dbname = "testdb"
+dburi = "mysql://{}:{}@{}/{}".format(dbuser, dbpasswd, dbserver, dbname)
+print("dburi: {}".format(dburi))
+
+# see here for basic SQLAlchemy start
+# http://flask-sqlalchemy.pocoo.org/2.3/quickstart/#quickstart
+# here for detailed Models examples and docs
+# http://flask-sqlalchemy.pocoo.org/2.3/models/#models
+# here for detailed data manipulations
+# http://flask-sqlalchemy.pocoo.org/2.3/queries/#inserting-records
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = dburi 
+db = SQLAlchemy(app)
 
+class User(db.Model):
+  __tablename__ = 't'
+  name = db.Column('c', db.CHAR(20))
+  uid = db.Column('ci', db.INTEGER, primary_key=True)
 
 #import WTForms
 from flask_wtf import FlaskForm
@@ -28,9 +50,16 @@ def renderMe():
 @app.route(u'/submit', methods=[u'POST'])
 def submitMethod():
   form = SubmitForm()
-  #if form.valudate_on_submit():
-  return_message = "text field:{} ; int field:{}".format(form.text_field, form.int_field)
-  return return_message
+  user_info = User(name=form.text_field.data, uid=form.int_field.data)
+
+  if form.validate_on_submit():
+   print("in if")
+   db.session.add(user_info)
+   db.session.commit()
+   return "text field:{} ; int field:{}".format(form.text_field.data, form.int_field.data)
+  print(form.errors)
+  print("after if")
+  return render_template('index.html', form=form)
     
 if __name__ == "__main__":
   app.run(host = '0.0.0.0')

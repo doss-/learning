0f106ba08a3a7cc8bff653d767f7c477e677bb9e
doss- <1234dos@gmail.com>
2018-08-01 02:15:44 +0300

    IT'S ALIVEgit add database/db_setup.sql !

--- Dockerfiles/docker-compose/first_try/database/db_setup.sql
+++ Dockerfiles/docker-compose/first_try/database/db_setup.sql
@@ -2,4 +2,4 @@
 grant all on *.* to 'tuser'@'localhost';
 create schema testdb;*/
 CREATE TABLE IF NOT EXISTS testdb.t (c CHAR(20), ci INT);
-INSERT testdb.t (c) VALUE ('first', 1);
+INSERT testdb.t (c, ci) VALUE ('first', 1);

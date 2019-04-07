c686cd4532df6a5c6fe14e725db35b40d579fb6a
doss- <1234dos@gmail.com>
2019-04-08 01:09:39 +0300

    markdown: bash_cheatsheet convertion

--- infra.sh
+++ infra.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#not ready at all yet
+exit 1
+
 #check whether we have universe enabled for htop 
 uname --machine  # will return architecture
 #match x86_x64 architecture
@@ -10,3 +13,13 @@ apt-cache policy | grep "archive\.ubuntu\.com.*xenial-updates\/universe.*i386"
 add-apt-repository universe
 #recheck if added, if not use long line
 #which probably is in linux_cheatsheet but in master branch 
+
+#install skype, check snap exists
+sudo snap install skype --classic
+
+#install docker + compose
+#check which OS before installing docker, and pick accordingly
+
+#install mkdocs
+
+#install apache for mkdocs hosting

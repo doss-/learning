01c82af3413c46498ba08f79dac37bd4190ea35b
doss- <1234dos@gmail.com>
2019-07-24 01:45:24 +0300

    jenkins-course: part 7. crumbs and web requests invocation by curl

---
+++ jenkins-course/jenkins-data/crumb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+crumb=$(curl -u "builder:1234" -s 'http://jenkins.local:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+curl -u "builder:1234" -H "$crumb" -X POST http://jenkins.local:8080/job/ENV/build?delay=0sec

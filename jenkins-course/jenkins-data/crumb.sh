b4e67686b02fd99572cf662af62b7e768a57e3a6
doss- <1234dos@gmail.com>
2019-07-27 13:17:26 +0300

    ssl public key encryption example, REST jenkins calls with Crumbs

--- jenkins-course/jenkins-data/crumb.sh
+++ jenkins-course/jenkins-data/crumb.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 crumb=$(curl -u "builder:1234" -s 'http://jenkins.local:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-curl -u "builder:1234" -H "$crumb" -X POST http://jenkins.local:8080/job/ENV/build?delay=0sec
+#curl -u "builder:1234" -H "$crumb" -X POST http://jenkins.local:8080/job/ENV/build?delay=0sec
+
+#curl -u "builder:1234" -H "$crumb" -X POST http://jenkins.local:8080/job/backup-to-aws/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=dos-deepdive
+
+curl -u "builder:1234" -H "$crumb" -X POST http://jenkins.local:8080/job/ansible-users-from-db/buildWithParameters?AGE=25

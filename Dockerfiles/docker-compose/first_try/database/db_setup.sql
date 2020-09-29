06548af8997d52c90e8b06afe3d912d586dfdfae
doss- <1234dos@gmail.com>
2020-09-29 04:23:12 +0300

    some old stuff removal

--- Dockerfiles/docker-compose/first_try/database/db_setup.sql
+++
@@ -1,5 +0,0 @@
-/*create user 'tuser'@'localhost' identified by 'testp';
-grant all on *.* to 'tuser'@'localhost';
-create schema testdb;*/
-CREATE TABLE IF NOT EXISTS testdb.t (c CHAR(20), ci INT);
-INSERT testdb.t (c, ci) VALUE ('first', 1);
